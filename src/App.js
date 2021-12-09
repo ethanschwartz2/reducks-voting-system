31abef819bc95d61ec184803f1bbc39ab855316a
asayyed <asiya_sayyed@intuit.com>
2021-12-09 10:06:21 -0800

    Corrected the port number

--- src/App.js
+++ src/App.js
@@ -4,36 +4,40 @@ import {
     Route,
     Link
 } from "react-router-dom";
-
-import './App.css';
-import { Provider } from 'react-redux';
+import {Register} from "./components/Register";
 import {Vote} from "./components/Vote";
-import {CreateElection} from "./components/CreateElection";
+import {CreateElection} from "./components/CreateElection/CreateElection";
+import {Layout} from "./Layout";
 import { voterToolStore } from "./stores/voterToolStore";
-import { Register } from "./components/Register";
+import { Provider } from "react-redux";
 
 function App() {
   return (
-      <BrowserRouter>
-          <nav>
-              <ul className="menu">
-                  <li className="menu-item"><Link to="/register">Register</Link></li>
-                  <li className="menu-item"><Link to="/vote">Vote</Link></li>
-                  <li className="menu-item"><Link to="/createElection">Create Election</Link></li>
-              </ul>
-          </nav>
-          <Routes>
-              <Route path="/register" element={
-                <Provider store={voterToolStore}>
-                    <Register />
-                </Provider>
-              }/>
-              <Route path="/vote" element={<Vote/>}/>
-              <Route path="/createElection" element={<CreateElection/>}/>
-              <Route path="*" element={<Vote/>}/>
-          </Routes>
-          <footer>Some Footer </footer>
-      </BrowserRouter>
+      <Layout>
+          <BrowserRouter>
+              <header><h1>ReDucks Voting System</h1></header>
+              <nav>
+                  <ul className="menu">
+                      <li className="menu-item"><Link to="/register">Register</Link></li>
+                      <li className="menu-item"><Link to="/vote">Vote</Link></li>
+                      <li className="menu-item"><Link to="/createElection">Create Election</Link></li>
+                  </ul>
+              </nav>
+              <main>
+                  <Routes>
+                        <Route path="/register" element={
+                            <Provider store={voterToolStore}>
+                                <Register />
+                            </Provider>
+                        }/>
+                      <Route path="/vote" element={<Vote/>}/>
+                      <Route path="/createElection" element={<CreateElection/>}/>
+                      <Route path="*" element={<Vote/>}/>
+                  </Routes>
+              </main>
+              <footer>Some Footer Text</footer>
+          </BrowserRouter>
+      </Layout>
   );
 }
 
