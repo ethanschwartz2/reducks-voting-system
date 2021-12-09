cf61d2d86cd08dceba8c35d5b6169e6385a5e9df
eschwartz <Ethan_schwartz2@intuit.com>
2021-12-09 13:28:00 -0700

    commit -m add in modal for createElection

--- src/components/CreateElection/CreateElection.js
+++ src/components/CreateElection/CreateElection.js
@@ -1,11 +1,37 @@
 import {CurrentElections} from "./CurrentElections";
+import {ElectionModal} from "./ElectionModal";
+import {useState, useEffect} from "react";
+import './CreateElectionLayout.css'
 
 export const CreateElection = () => {
+    const [showModal, setShowModal] = useState(false)
+
+    useEffect(()=> {
+
+    },[showModal])
+
+    const openModal = () => {
+        setShowModal(true);
+    }
+    const closeModal = () => {
+        setShowModal(false);
+    }
+
+    const saveElection =()=> {
+
+    }
+
+
     return(
-        <>
-            <h2>Current Elections</h2>
-            <CurrentElections/>
-            <h2>Create An Election</h2>
-    </>
+        <div className="CreateElection">
+            <header>
+                <h2>Current Elections</h2>
+                <button className="CreateElectionButton" type="button" onClick={openModal}>Create An Election</button>
+            </header>
+            <hr/>
+                <CurrentElections/>
+                {showModal ? <ElectionModal close={closeModal} saveElection={saveElection}/> : null}
+
+    </div>
     );
 }
