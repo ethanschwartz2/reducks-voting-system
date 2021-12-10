3c3fdecbe9860121064a3a8c4eb3f6823ca5aeed
eschwartz <Ethan_schwartz2@intuit.com>
2021-12-09 18:33:08 -0700

    commit -m connect CurrentElections to redux

---
+++ src/actions/createElectionActions.js
@@ -0,0 +1,34 @@
+import {allElections, appendElection} from "../apis/elections";
+
+export const CURRENT_ELECTIONS_REQUEST_ACTION = 'CURRENT_ELECTIONS_REQUEST';
+export const CURRENT_ELECTIONS_DONE_ACTION = 'CURRENT_ELECTIONS_DONE';
+
+export const CREATE_ELECTION_REQUEST_ACTION = 'CREATE_ELECTION_REQUEST';
+
+export const createCurrentElectionsRequestAction = () => ({
+    type: CURRENT_ELECTIONS_REQUEST_ACTION});
+export const createCurrentElectionsDoneAction = (value) => ({
+    type: CURRENT_ELECTIONS_DONE_ACTION, payload: { value }});
+
+export const createElectionRequestAction = election => ({
+    type: CREATE_ELECTION_REQUEST_ACTION, payload: { election }
+});
+
+export const getElections = () => {
+    // get the election
+    console.log('done');
+    return dispatch => {
+        dispatch(createCurrentElectionsRequestAction());
+        return allElections().then(value => {
+            dispatch(createCurrentElectionsDoneAction(value));
+        });
+    };
+};
+
+export const addElection = election => {
+    return dispatch => {
+        dispatch(createElectionRequestAction(election))
+        appendElection(election).then(()=> dispatch(getElections()))
+    }
+}
+
--- src/reducers/voteSystemReducers.js
+++ src/reducers/voteSystemReducers.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import { isLoadingReducer } from "./loadingReducer";
 import { ballotReducer } from "./ballotReducers";
 import { voterToolReducer } from "./voterToolReducer";
+import { electionReducer } from "./electionReducer";
 
 export const voteToolReducer = combineReducers({
     isLoading: isLoadingReducer,
     voter: voterToolReducer,
     ballot: ballotReducer,
+    elections: electionReducer,
 })
